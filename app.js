96495c8145fc654aea5ada652b74a2bd9ed5e174
yo simik <167876439+letargicky@users.noreply.github.com>
2025-01-23 22:05:25 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const crypto = require('crypto');
 const cors = require('cors');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const { put, get } = require('@vercel/blob');
 
 const app = express();
