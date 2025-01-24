d23513e73ab12229857c51e04e9da3ef07b87d90
yo simik <167876439+letargicky@users.noreply.github.com>
2025-01-24 07:48:54 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const crypto = require('crypto');
 const cors = require('cors');
+const fetch = require('node-fetch');
 
 const app = express();
 const port = 8000;
